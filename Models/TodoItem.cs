a9b82940c407a1fb8941d2b3a48d0a2a1740eb56
Ricardo <ricardo.demauro@outlook.com>
2020-03-19 23:56:07 -0300

    initial commit

---
+++ Models/TodoItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.TodoList.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+
+        public string Task { get; set; }
+
+        public bool Done { get; set; }
+
+        public TodoItem(Guid id, string task)
+        {
+            Id = id;
+            Task = task;
+        }
+
+        public TodoItem(string task)
+            : this(Guid.NewGuid(), task)
+        {
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+﻿# Markdown File
+
+Layout from https://bbbootstrap.com/snippets/todo-list-template-local-storage-10214124
